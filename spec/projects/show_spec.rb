f790231cfb0d9f0103102150952f3db205472d08
Danny Ramos <danramos21@gmail.com>
2020-05-15 10:54:17 -0600

    Updated

--- spec/projects/show_spec.rb
+++ spec/projects/show_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe "When I visit a project's show page '/projects/:id'" do
   it "I see that project's name and material" do
     project_1 = Project.create(name: "Litfit", material: "Lamp Shade")
-    challenge_1 = project_1.challenges.create(theme: "Apartment Furnishings")
+    challenge_1 = Challenge.create(theme: "Apartment Furnishings")
 
     visit "/projects/#{project_1.id}"
-
+    
     expect(page).to have_content(project_1.name)
     expect(page).to have_content("Material: #{project_1.material}")
     expect(page).not_to have_content("Challenge Theme: #{challenge_1.theme}")
