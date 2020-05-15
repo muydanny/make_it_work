4aae07e8e54204271343b8720c049ce56e2d22a7
Danny Ramos <danramos21@gmail.com>
2020-05-15 10:40:24 -0600

    Independent Challenge

---
+++ spec/projects/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit a project's show page '/projects/:id'" do
+  it "I see that project's name and material" do
+    project_1 = Project.create(name: "Litfit", material: "Lamp Shade")
+    challenge_1 = project_1.challenges.create(theme: "Apartment Furnishings")
+
+    visit "/projects/#{project_1.id}"
+
+    expect(page).to have_content(project_1.name)
+    expect(page).to have_content("Material: #{project_1.material}")
+    expect(page).not_to have_content("Challenge Theme: #{challenge_1.theme}")
+  end
+end
+
+
+
+
+
+# I see that project's name and material
+# And I also see the theme of the challenge that this project belongs to.
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings)
